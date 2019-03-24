1e383c677e5635ed3132e7bb394ad23faadadb65
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-03-24 11:10:49 +0100

    Pseudo-code to create dID

--- app/javascript/didGenerate.js
+++ app/javascript/didGenerate.js
@@ -1,8 +1,21 @@
+/*
+To create a DecentralizedID:
+Deploy AlastriaIdentityManager
+Call function createIdentity
+Event LogIdentityCreated identity is the address of the proxy
+*/
+
+import * as bcWeb3 from './blockchainWeb3/bcWeb3.js';
+//import {deployContract} from './blockchainWeb3/bcWeb3.js';
+
+let contractPath = './../../contracts/newIdentityManager/AlastriaIdentityManager.sol';
+let contractName = 'AlastriaIdentityManager';
+
 let didModel = {
 
 }
 
 function generate(){
-
+    bcWeb3.deployContract(contractPath, contractName);
     return
 }
