abc3535e66677072b034e131ced499f381fefeee
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-03-25 17:20:53 +0100

    generateDID and idManagerLib

--- app/javascript/didGenerate.js
+++ app/javascript/didGenerate.js
@@ -1,12 +1,13 @@
 /*
 To create a DecentralizedID:
 Deploy AlastriaIdentityManager
+call function generateAccessToken
 Call function createIdentity
 Event LogIdentityCreated identity is the address of the proxy
 */
 
 import * as bcWeb3 from './blockchainWeb3/bcWeb3.js';
-//import {deployContract} from './blockchainWeb3/bcWeb3.js';
+import * as idManagerWeb3 from './blockchainWeb3/idManagerWeb3.js';
 
 let contractPath = './../../contracts/newIdentityManager/AlastriaIdentityManager.sol';
 let contractName = 'AlastriaIdentityManager';
@@ -16,6 +17,9 @@ let didModel = {
 }
 
 function generate(){
-    var identiyManager = bcWeb3.deployIdentityManager(1);
+    var defaultAccount = bcWeb3.getDefaultAccount();
+    var identityManager = idManagerWeb3.deployIdentityManager(1);
+    idManagerWeb3.generateAccessToken(identityManager);
+    idManagerWeb3.createAlastriaIdentity(identityManager);
     return
 }
