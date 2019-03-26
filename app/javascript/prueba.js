62f1e892cc3cdcbe655c69d321a303b726be8403
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-03-26 17:40:07 +0100

    Front to interact with functions

--- app/javascript/prueba.js
+++ app/javascript/prueba.js
@@ -3,16 +3,16 @@ import * as bcWeb3 from './blockchainWeb3/bcWeb3.js';
 import * as idManagerWeb3 from './blockchainWeb3/idManagerWeb3.js';
 import * as registryWeb3 from './blockchainWeb3/registryWeb3.js';
 
-var acc = bcWeb3.getAccounts();
+/*var acc = bcWeb3.getAccounts();
 console.log(acc)
 var def1 = bcWeb3.getDefaultAccount();
 console.log(def1)
 bcWeb3.setDefaultAccount(acc[3]);
 var def2 = bcWeb3.getDefaultAccount();
 console.log(def2)
+*/
 
 var randomAddress = "0xc0d8f541ab8b71f20c10261818f2f401e8194049";
 var pubKeyRegistry = registryWeb3.deployPubKeyRegistry(randomAddress);
 
 var identityManager = idManagerWeb3.deployIdentityManager(1);
-console.log(identityManager.address)
