49524a91ce8984fa4beb9ac3113f00fe4b164c6a
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-03-29 14:35:56 +0100

    Fix the problem verifying

--- app/javascript/createSignature.js
+++ app/javascript/createSignature.js
@@ -4,14 +4,15 @@ var readlineSync = require('readline-sync');
 
 var idName = readlineSync.question("Enter ID name: ");
 const privateKey = fs.readFileSync('./../ids/' + idName + '/did/privateKey.pem', 'utf8');
+console.log(privateKey)
 
 var data = readlineSync.question("Data to sign ¡CAMBIAR POR FICHERO!: ");
 const sign = crypto.createSign('SHA256');
 sign.update(data);
 sign.end();
 const signature = sign.sign(privateKey);
-console.log(signature.toString('base64'))
-fs.writeFile('./../ids/' + idName + '/did/signature.txt', signature.toString('base64'), 'utf8', (err) => {
+console.log(signature.toString('hex'))
+fs.writeFile('./../ids/' + idName + '/did/signature.txt', signature.toString('hex'), 'utf8', (err) => {
   if(err) throw err;
   console.log('signature saved')
 });
--- app/javascript/verifySignature.js
+++ app/javascript/verifySignature.js
@@ -5,9 +5,11 @@ var readlineSync = require('readline-sync');
 var idName = readlineSync.question("Enter ID name: ");
 const publicKey = fs.readFileSync('./../ids/' + idName + '/did/publicKey.pem', 'utf8');
 var plainData = readlineSync.question("plainData: ");
-//var signature = fs.readFileSync('./../ids/' + idName + '/did/signature.txt', 'utf8');
-var signature = readlineSync.question("signature: ");
+var signature = fs.readFileSync('./../ids/' + idName + '/did/signature.txt', 'utf8');
+//var signature = readlineSync.question("signature: ");
 const verify = crypto.createVerify('SHA256');
 verify.update(plainData);
 verify.end();
-console.log(verify.verify(publicKey, signature));
+const publicKeyBuf = new Buffer.from(publicKey, 'ascii')
+const signatureBuf = new Buffer.from(signature, 'hex')
+console.log(verify.verify(publicKeyBuf, signatureBuf));
