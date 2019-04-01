e41891edf14cd966eb6728e0b31e9f044e73f223
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-04-01 13:21:11 +0200

    Create directory for id and file with alastriaID

--- app/javascript/createDID.js
+++ app/javascript/createDID.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var readlineSync = require('readline-sync');
 const bcWeb3 = require('./blockchainWeb3/bcWeb3.js');
 const idManagerWeb3 = require('./blockchainWeb3/idManagerWeb3.js');
 var Web3 = require('web3');
@@ -9,7 +10,8 @@ var identityManagerAddress = "0x4a41672e9a217c3193b12483400b03ce1851e145";
 var accounts = bcWeb3.getAccounts();
 bcWeb3.setDefaultAccount(accounts[0])
 
-module.exports.createAlastriaIdentity = async function(idName){
+module.exports.createAlastriaIdentity = async function(){
+  var idName = readlineSync.question("Enter ID name: ");
   dir = './../ids/' + idName;
   if(!fs.existsSync(dir)){
     fs.mkdirSync(dir);
@@ -23,10 +25,6 @@ module.exports.createAlastriaIdentity = async function(idName){
   idManagerWeb3.listenAccessTokenEvent(identityManagerAddress, accounts[1])
   idManagerWeb3.createAlastriaIdentity(identityManagerAddress)
   idManagerWeb3.listenLogIdentityCreatedEvent(identityManagerAddress, accounts[1], idName)
-}
-
-module.exports.events = function(){
-  bcWeb3.printEventLog(LogIdentityCreated)
 }
 
 require('make-runnable/custom')({
